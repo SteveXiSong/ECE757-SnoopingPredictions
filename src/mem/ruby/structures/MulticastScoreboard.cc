22b5ef4d15edcf0a3ec882b839f05c0bd69aefe7
Edward L Tashjian <etashjia@tux-62.cae.wisc.edu>
2015-04-24 18:29:02 -0500

    Added initial version of multicast scoreboard

--- src/mem/ruby/structures/MulticastScoreboard.cc
+++ src/mem/ruby/structures/MulticastScoreboard.cc
@@ -5,6 +5,57 @@
 using namespace std;
 typedef MulticastScoreboardParams Params;
 
+MulticastScoreboard::MulticastScoreboard(const Params *p) : SimObject(p) 
+{
+  GETS_count = 0;
+  GETX_count = 0;
+  total_nodes_predicted = 0;
+  extra_nodes_predicted = 0;
+}; 
+
+void MulticastScoreboard::record_GETS(NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner) 
+{
+  GETS_count++; 
+}
+
+void MulticastScoreboard::record_GETS(NetDest pred_set) 
+{
+  GETS_count++; 
+}
+
+void MulticastScoreboard::record_GETX(NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner)
+{
+  GETX_count++;
+}
+
+void MulticastScoreboard::record_GETX(NetDest pred_set)
+{
+  GETX_count++;
+}
+
+
+void MulticastScoreboard::regStats()
+{
+    GETS_count
+        .name(name() + ".GETS_count")
+        .desc("Number of multicast GETS's received")
+        ;
+
+    GETX_count
+        .name(name() + ".GETX_count")
+        .desc("Number of multicast GETX's received")
+        ;
+
+    total_nodes_predicted
+        .name(name() + ".total_nodex_predicted")
+        .desc("Total count of nodes predicted in multicasts")
+        ;
+
+    extra_nodes_predicted
+        .name(name() + ".extra_nodex_predicted")
+        .desc("Total count extra unecessary nodes predicted in multicasts")
+        ;
+}
 
 MulticastScoreboard *
 MulticastScoreboardParams::create()
--- src/mem/ruby/structures/MulticastScoreboard.hh
+++ src/mem/ruby/structures/MulticastScoreboard.hh
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdlib.h>
 
+#include "base/statistics.hh"
 #include "mem/ruby/common/NetDest.hh"
 #include "mem/ruby/common/Address.hh"
 #include "mem/protocol/MachineType.hh"
@@ -13,10 +14,31 @@
 
 class MulticastScoreboard : public SimObject {
  public:
-  MulticastScoreboard(const Params *p) : SimObject(p) {}; 
+  MulticastScoreboard(const Params *p); 
+
+  // Registers statistics for a multicast GETS operation where local directory entry exists
+  void record_GETS(NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner);
+
+  // Registers statistics for a multicast GETS operation where local directory entry doesn't exist
+  void record_GETS(NetDest pred_set);
+
+  // Registers statistics for a multicast GETX operation where local directory entry exists
+  void record_GETX(NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner);
+  
+  // Registers statistics for a multicast GETX operation where local directory entry doesn't exist
+  void record_GETX(NetDest pred_set);
+
+  // FOR STAT HANDLING
+  void regStats();
 
   // MANDATORY SIM OBJECT METHODS
   MulticastScoreboard& operator=(const MulticastScoreboard& obj);
+
+ private:
+  Stats::Scalar GETS_count;
+  Stats::Scalar GETX_count;
+  Stats::Scalar total_nodes_predicted;
+  Stats::Scalar extra_nodes_predicted;
 };
 
 #endif // __MEM_RUBY_STRUCTURE_MULTICASTSCOREBOARD_HH__))))
