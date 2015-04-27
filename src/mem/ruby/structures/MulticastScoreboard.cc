af64412cd84846a9fdfc2fb31f61e9b3b4d1237b
Edward L Tashjian <etashjia@tux-62.cae.wisc.edu>
2015-04-27 14:50:26 -0500

    Updated scoreboard
    Conflicts:
    	src/mem/protocol/MOESI_CMP_directory-L2cache.sm

--- src/mem/ruby/structures/MulticastScoreboard.cc
+++ src/mem/ruby/structures/MulticastScoreboard.cc
@@ -10,8 +10,12 @@ MulticastScoreboard::MulticastScoreboard(const Params *p) : SimObject(p)
 {
   GETS_count = 0;
   GETX_count = 0;
+  PUTX_count = 0;
   total_nodes_predicted = 0;
   extra_nodes_predicted = 0;
+  perfect_nodes_predicted = 0;
+  accurate_predictions = 0;
+  l2_predictions = 0;
 }; 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -22,6 +26,7 @@ void MulticastScoreboard::record_GETS(MachineID l1, NetDest pred_set, NetDest sh
   // update basic counts
   GETS_count++; 
   total_nodes_predicted += pred_set.count();
+  perfect_nodes_predicted += (owner_valid && owner.type == MachineType_L1Cache ? 3 : 2);
 
   // only necessary L1 nodes are itself and owner
   for (NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
@@ -29,6 +34,14 @@ void MulticastScoreboard::record_GETS(MachineID l1, NetDest pred_set, NetDest sh
     if (mach != l1 && pred_set.isElement(mach) && (!owner_valid || mach != owner)) {
       extra_nodes_predicted++;
     }
+  }
+
+  // update prediction accuracy data
+  if(!owner_valid || owner.type == MachineType_L2Cache) {
+    accurate_predictions++;
+    l2_predictions++;
+  } else if(pred_set.isElement(owner)) {
+    accurate_predictions++;
   }
 }
 
@@ -40,6 +53,7 @@ void MulticastScoreboard::record_GETS(MachineID l1, NetDest pred_set)
   // update basic counts
   GETS_count++;
   total_nodes_predicted += pred_set.count();
+  perfect_nodes_predicted += 2;
 
   // any L1s predicted in this case are extra since L2 owns this data
   for (NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
@@ -48,6 +62,10 @@ void MulticastScoreboard::record_GETS(MachineID l1, NetDest pred_set)
       extra_nodes_predicted++;
     }
   }
+
+  // update prediction accuracy data
+  accurate_predictions++;
+  l2_predictions++;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -58,6 +76,7 @@ void MulticastScoreboard::record_GETX(MachineID l1, NetDest pred_set, NetDest sh
   // update basic counts
   GETX_count++;
   total_nodes_predicted += pred_set.count();
+  perfect_nodes_predicted += (owner_valid && owner.type == MachineType_L1Cache ? 3 : 2) + sharers.count();
 
   // necessary L1 are itslef, sharers and an owner if it exists
   for (NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
@@ -66,6 +85,17 @@ void MulticastScoreboard::record_GETX(MachineID l1, NetDest pred_set, NetDest sh
       extra_nodes_predicted++;
     }
   }
+
+  // update accuracy counts
+  if(sharers.isSubset(pred_set)) {
+    if(!owner_valid || owner.type == MachineType_L2Cache) {
+      accurate_predictions++;
+      l2_predictions++;
+    } else if(pred_set.isElement(owner)) {
+      accurate_predictions++;
+    }
+  }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -76,6 +106,7 @@ void MulticastScoreboard::record_GETX(MachineID l1, NetDest pred_set)
   // update basic counts
   GETX_count++;
   total_nodes_predicted += pred_set.count();
+  perfect_nodes_predicted += 2;
 
   // any L1s predicted in this case are extra since L2 owns this data
   for (NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
@@ -84,6 +115,61 @@ void MulticastScoreboard::record_GETX(MachineID l1, NetDest pred_set)
       extra_nodes_predicted++;
     }
   }
+
+  // update accuracy counts
+  accurate_predictions++;
+  l2_predictions++; 
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void MulticastScoreboard::record_PUTX(MachineID l1, NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner)
+{
+  assert(l1.type == MachineType_L1Cache);
+
+  // update basic counts
+  PUTX_count++;
+  total_nodes_predicted += pred_set.count();
+  perfect_nodes_predicted += (owner_valid && owner.type == MachineType_L1Cache ? 3 : 2) + sharers.count();
+
+  for(NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
+    MachineID mach = {MachineType_L1Cache, i};
+    if (l1 != mach && pred_set.isElement(mach) && !sharers.isElement(mach)) {
+      extra_nodes_predicted++;
+    }
+  }
+
+  // update accuracy counts
+  if(sharers.isSubset(pred_set)) {
+    if(!owner_valid || owner.type == MachineType_L2Cache) {
+      accurate_predictions++;
+      l2_predictions++;
+    } else if(pred_set.isElement(owner)) {
+      accurate_predictions++;
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////  
+void MulticastScoreboard::record_PUTX(MachineID l1, NetDest pred_set)
+{
+  assert(l1.type == MachineType_L1Cache);
+
+  // update basic counts
+  PUTX_count++;
+  total_nodes_predicted += pred_set.count(); 
+  perfect_nodes_predicted += 2;
+
+  // any L1s predicted in this case are extra since L2 owns this data
+  for(NodeID i = 0; i < MachineType_base_count(MachineType_L1Cache); i++) {
+    MachineID mach = {MachineType_L1Cache, i};
+    if (l1 != mach && pred_set.isElement(mach)) {
+      extra_nodes_predicted++;
+    }
+  }
+
+  // update accuracy counts
+  accurate_predictions++;
+  l2_predictions++; 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -99,15 +185,66 @@ void MulticastScoreboard::regStats()
         .desc("Number of multicast GETX's received")
         ;
 
+    PUTX_count
+        .name(name() + ".PUTX_count")
+        .desc("Number of multicast PUTX's received")
+        ;
+
     total_nodes_predicted
-        .name(name() + ".total_nodex_predicted")
+        .name(name() + ".total_nodes_predicted")
         .desc("Total count of nodes predicted in multicasts")
         ;
 
     extra_nodes_predicted
-        .name(name() + ".extra_nodex_predicted")
+        .name(name() + ".extra_nodes_predicted")
         .desc("Total count extra unecessary nodes predicted in multicasts")
         ;
+    
+    perfect_nodes_predicted
+        .name(name() + ".perfect_nodes_predicted")
+        .desc("Total count of nodes predicted with perfect multicasts")
+        ;
+
+    accurate_predictions
+        .name(name() + ".accurate_predictions")
+        .desc("Total count of accurately predicted multicasts")
+        ;
+
+    l2_predictions
+        .name(name() + ".l2_predictions")
+        .desc("Total count of accurately predicted multicast servied by l2")
+        ;
+
+    // Recorded Stats
+    avg_nodes_in_multicast
+        .name(name() + ".avg_nodes_in_multicast")
+        .desc("Avg. number of nodes included in multicasts")
+        ;
+    avg_nodes_in_multicast = (total_nodes_predicted / (PUTX_count + GETX_count + GETS_count));
+
+    avg_extra_nodes_predicted
+        .name(name() + ".avg_extra_nodes_predicted")
+        .desc("Avg. number of extra (uncessary) nodes included in multicasts")
+        ;
+    avg_extra_nodes_predicted = (extra_nodes_predicted / (PUTX_count + GETX_count + GETS_count));
+  
+    multicast_traffic_ratio
+        .name(name() + ".multicast_traffic_ratio")
+        .desc("Ratio of total nodes predicted to nodes predicted in multicast to nodes in perfect multicast")
+        ;
+    multicast_traffic_ratio = total_nodes_predicted / perfect_nodes_predicted;
+
+    prediction_accuracy
+        .name(name() + ".prediction_accuracy")
+        .desc("Percent accuracy of predictions made by L1's in system")
+        ;
+    prediction_accuracy = accurate_predictions / (PUTX_count + GETX_count + GETS_count);
+
+    blocks_found_at_l2
+        .name(name() + ".block_found_at_l2")
+        .desc("Percent of blocks that were serviced by l2")
+        ;
+    blocks_found_at_l2 = l2_predictions / (PUTX_count + GETX_count + GETS_count);
 }
 
 MulticastScoreboard *
--- src/mem/ruby/structures/MulticastScoreboard.hh
+++ src/mem/ruby/structures/MulticastScoreboard.hh
@@ -28,6 +28,13 @@ class MulticastScoreboard : public SimObject {
   // Registers statistics for a multicast GETX operation where local directory entry doesn't exist
   void record_GETX(MachineID l1, NetDest pred_set);
 
+  // Registers statistics for a multicast PUTX operation where local directory entry exists
+  void record_PUTX(MachineID l1, NetDest pred_set, NetDest sharers, bool owner_valid, MachineID owner);
+  
+  // Registers statistics for a multicast PUTX operation where local directory entry doesn't exist
+  void record_PUTX(MachineID l1, NetDest pred_set);
+  
+
   // FOR STAT HANDLING
   void regStats();
 
@@ -35,10 +42,22 @@ class MulticastScoreboard : public SimObject {
   MulticastScoreboard& operator=(const MulticastScoreboard& obj);
 
  private:
+  // Tracking Stats
   Stats::Scalar GETS_count;
   Stats::Scalar GETX_count;
+  Stats::Scalar PUTX_count;
   Stats::Scalar total_nodes_predicted;
   Stats::Scalar extra_nodes_predicted;
+  Stats::Scalar perfect_nodes_predicted;
+  Stats::Scalar accurate_predictions;
+  Stats::Scalar l2_predictions;
+
+  // Recorded Stats
+  Stats::Formula avg_nodes_in_multicast;
+  Stats::Formula avg_extra_nodes_predicted;
+  Stats::Formula multicast_traffic_ratio;
+  Stats::Formula prediction_accuracy;
+  Stats::Formula blocks_found_at_l2;
 };
 
 #endif // __MEM_RUBY_STRUCTURE_MULTICASTSCOREBOARD_HH__
